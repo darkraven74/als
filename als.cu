70a74dcb1aa3689891535cbcc97c2b210c4a8fb8
Vladimir Baev <darkraven8@gmail.com>
2015-08-12 19:44:21 +0300

    mse fixed

--- als.cu
+++ als.cu
@@ -355,8 +355,8 @@ void als::draw_samples_for_error(features_vector& users, features_vector& items,
      {
          for( int i=0;  i < _count_error_samples_for_users; i++)
          {         
-             const int r1 = rand() % _count_users;
-//           const int r1 = i;
+//             const int r1 = rand() % _count_users;
+           const int r1 = i;
              users_for_error.push_back(r1);
          }
      }
@@ -374,8 +374,8 @@ void als::draw_samples_for_error(features_vector& users, features_vector& items,
      {
          for( int i=0;  i < _count_error_samples_for_items; i++)
          {         
-             const int r1 = rand() % _count_items;
-//           const int r1 = i;
+//             const int r1 = rand() % _count_items;
+           const int r1 = i;
              items_for_error.push_back(r1);
          }
      }
@@ -1495,6 +1495,7 @@ void als::calc_error()
   thrust::copy(x_device.begin(), x_device.end(), mat.begin());
   for(int i=0; i < _count_error_samples_for_users * _count_error_samples_for_items; i++)                       
   {
+      if (r[i] == 1)
         error += (r[i] - mat[i]) *  ( r[i] - mat[i] );
 //        error += (1 - mat[i]) *  ( 1 - mat[i] );
   }
